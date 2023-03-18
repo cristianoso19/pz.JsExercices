a65b0eca721e79170b4cdd26eff0b1f619a54587
CristianLenovoU22 <cristianoso19@gmail.com>
2023-03-18 01:57:49 -0500

    Class average

--- ClassAverage.js
+++ ClassAverage.js
@@ -1,31 +1,81 @@
-function getStudentAverage(students) {
-    // Tu código aquí 👈
-    class Student {
-      constructor({
-        name,
-        grades,
-      }) {
-        this.name = name;
-        this.grades = [grades];
-        this.average = 0
-      }
-      classAverage(grades) {
-        let average = 0;
-        console.log(typeof grades);
-      }  
-    };
-  
-    let groupAverage = students.forEach(function (name, grades) {
-      const student = new Student(name, grades);
-      console.log(student.classAverage(grades));
-      return student.classAverage();
-    })
-  
-    console.log(groupAverage);
-    return groupAverage;
-  }
+/* En este desafío, deberás calcular el promedio general de una clase, así como el promedio individual de cada estudiante.
 
-   getStudentAverage([
+Para ello, se te proporcionará un array de objetos, cada uno de los cuales representará a un estudiante y tendrá las siguientes propiedades:
+
+name: El nombre del estudiante
+grades: Las notas de cada materia del estudiante
+A partir de esta información, debes retornar un nuevo objeto que tenga la propiedad classAverage con el promedio de la clase y un array de students con los estudiantes y sus promedios individuales.
+
+Es importante mencionar que los promedios deben ser calculados con precisión y se deben redondear a dos decimales para que los test pasen sin problema alguno. Puedes usar el método toFixed() el cual se usa de la siguiente manera 👇
+
+const number = 100.32433;
+number.toFixed(2); // "100.32"
+
+👀 Ten en cuenta que este método regresa el número como un string y se espera que sea de tipo numérico.
+
+Ejemplo:
+
+Input: getStudentAverage([
+  {
+    name: "Pedro",
+    grades: [90, 87, 88, 90],
+  },
+  {
+    name: "Jose",
+    grades: [99, 71, 88, 96],
+  },
+  {
+    name: "Maria",
+    grades: [92, 81, 80, 96],
+  },
+])
+
+Output: {
+  classAverage: 88.17,
+  students: [
+    {
+      name: "Pedro",
+      average: 88.75
+    },
+    {
+      name: "Jose",
+      average: 88.5
+    },
+    {
+      name: "Maria",
+      average: 87.25
+    }
+  ]
+} */
+
+function getStudentAverage(students) {
+  // Tu código aquí 👈
+  let classSum=0;
+  let newStudents = [];
+  let studentsAverage = students.map((student) => {
+    let sum = 0;
+    student.grades.forEach((grade) => {
+      sum += grade;
+    });
+    const newStudent = {
+      name: student.name,
+      average: sum / student.grades.length,
+    };
+
+    classSum += newStudent.average;
+    return newStudent;
+  });
+
+  let classAverage = classSum / studentsAverage.length;
+  newStudents = {
+    classAverage: Number(classAverage.toFixed(2)),
+    students: studentsAverage 
+  }
+ console.log(newStudents); 
+ return newStudents
+}
+
+getStudentAverage([
   {
     name: "Pedro",
     grades: [90, 87, 88, 90],
